0b8d338f20fcb633ff58b7cb9f4ffe40daef18cc
jasogwa <jasogwa@nanocosmos.net>
2023-10-02 20:12:21 +0200

    (error):repo error checking

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -2,18 +2,23 @@ import config from './config';
 
 export const renderRepositoryInfo = (app, data) => {
     const repositoryInfo = document.createElement('div');
-    repositoryInfo.innerHTML = `
-    <div class="max-w-md mx-auto bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
-      <div class="flex items-center mb-4">
-        <img src="${data.owner.avatar_url}" alt="Repository Owner Avatar" class="w-8 h-8 mr-2 rounded-full">
-        <h2 class="text-xl font-bold">${data.name}</h2>
-      </div>
-      <p class="mb-2">Author: ${data.owner.login}</p>
-      <p class="mb-2">Stars: ${data.stargazers_count}</p>
-      <p class="mb-2">Forks: ${data.forks_count}</p>
-      <p>Popular: ${isPopular(data)}</p>
-    </div>
-  `;
+
+    if (data && data.owner) {
+        repositoryInfo.innerHTML = `
+            <div class="max-w-md mx-auto bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
+            <div class="flex items-center mb-4">
+                <img src="${data.owner.avatar_url}" alt="Repository Owner Avatar" class="w-8 h-8 mr-2 rounded-full">
+                <h2 class="text-xl font-bold">${data.name}</h2>
+            </div>
+            <p class="mb-2">Author: ${data.owner.login}</p>
+            <p class="mb-2">Stars: ${data.stargazers_count}</p>
+            <p class="mb-2">Forks: ${data.forks_count}</p>
+            <p>Popular: ${isPopular(data)}</p>
+            </div>
+        `;
+    } else {
+        repositoryInfo.innerHTML = 'Error: Unable to retrieve repository information.';
+    }
     app.appendChild(repositoryInfo);
 };
 
